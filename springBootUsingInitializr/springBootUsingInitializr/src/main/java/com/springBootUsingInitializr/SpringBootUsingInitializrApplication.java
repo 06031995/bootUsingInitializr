633e57cc909ee69550a168d978cbea492ea97bce
06031995 <patil3948@gmail.com>
2024-01-19 17:22:21 +0530

    second commit

---
+++ springBootUsingInitializr/springBootUsingInitializr/src/main/java/com/springBootUsingInitializr/SpringBootUsingInitializrApplication.java
@@ -0,0 +1,13 @@
+package com.springBootUsingInitializr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootUsingInitializrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootUsingInitializrApplication.class, args);
+	}
+
+}
---
+++ springBootUsingInitializr/springBootUsingInitializr/src/test/java/com/springBootUsingInitializr/SpringBootUsingInitializrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springBootUsingInitializr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootUsingInitializrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
